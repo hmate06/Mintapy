24930b59fba88fde808b298721fdb95e496f945a
hmate06 <horvath.mate3@students.jedlik.eu>
2024-01-24 16:35:50 +0100

    mintapy befejezve

--- mintapy.py
+++ mintapy.py
@@ -1,22 +1,39 @@
 import pygame
 
-# inicializálás - kötelező elem
+PIROS = (255, 0, 0)
+KEK = (0, 0, 255)
+SZIN = (54, 183, 93)
+FEHER = (255, 255, 255)
+
 pygame.init()
 
-# játékablak létrehozása
-# dupla () --> "tuple" Több érték tárolása egy változón belül
-ablak = pygame.display.set_mode((600, 300))
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('Mintapy')
 
-# - ablak címének megváltoztatása
-pygame.display.set_caption('Alapok')
+háttér_színe = SZIN
 
-# gameloop és eseményei
 fut = True
 while fut:
-    for esemény in pygame.event.get():      # bejárjuk az összes történő eseményt
-        if esemény.type == pygame.QUIT:     # ha az esemény bekövetkezik a while ciklusunk(gameloopunk) véget ér
+    for event in pygame.event.get():      
+        if event.type == pygame.QUIT:     
             fut = False
-    pygame.display.update()     # játék ablak frissítése
 
-# quit függvény hívása
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r: 
+                háttér_színe = PIROS
+            elif event.key == pygame.K_t: 
+                háttér_színe = KEK
+            elif event.key == pygame.K_z:
+                háttér_színe = SZIN
+
+    screen.fill(háttér_színe)
+
+    pygame.draw.circle(screen, FEHER, (400, 300), 100)
+    pygame.draw.rect(screen, (255, 0, 0), (30, 30, 150, 75))
+    pygame.draw.ellipse(screen, KEK, (50, 50, 200, 75))
+    pygame.draw.polygon(screen, KEK, [(120, 90), (120,200), (200, 90)])
+
+    pygame.display.update()     
+
+
 pygame.quit()
