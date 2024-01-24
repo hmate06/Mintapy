b0c38e55a04ac321e9b7064406c2e1512696ff75
BoriszaZsigmondBenedek <borisza.zsigmond.benedek@students.jedlik.eu>
2024-01-24 13:14:30 +0100

    alakzatok, színek kész

--- szinek.py
+++ szinek.py
@@ -1,15 +1,35 @@
 import pygame
 
+# háttérszínhez a constansok meghatározása
+PIROS = (255, 0, 0)
+KEK = (0,0,255)
+SZIN = (54, 183, 93)
+
 pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Színek")
 
-ablak = pygame.display.set_mode((600, 300))
-pygame.display.set_caption('Alapok')
+# hatter_szine = SZIN --- háttér színezése constanssal
 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-fut = True
-while fut:
-    for esemény in pygame.event.get():
-        fut = False
+        # Billentyű lenyomásra háttérszín megváltozása
+        if event.type == pygame.KEYDOWN: 
+
+            if event.key == pygame.K_r: # "r" billentyű lenyomása - piros lesz a háttér
+                hatter_szine = PIROS
+
+            elif event.key == pygame.K_t: # "t" billentyű lenyomása - kék lesz a háttér
+                hatter_szine = KEK
+
+    # --- háttér színezése constanssal
+    screen.fill(SZIN)
+    # screen.fill((R, G, B)) --- háttér színezése RGB kóddal
+
     pygame.display.update()
 
 pygame.quit()
